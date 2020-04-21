5a95154a699ae027f7b86dbcf7828908f74db751
DevGemmy <anthonia@nugitech.com>
2020-04-22 00:38:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all('/**', (req, res, next) => {
+  res.json(process.env);
+})
+
+app.listen(4000, () => {
+  console.log(process.env.ENV);
+})
