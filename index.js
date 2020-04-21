6bb0bae7e6291fd11821da86d2379c501cd6a64b
DevGemmy <anthonia@nugitech.com>
2020-04-22 00:51:21 +0100

    connect database and link user routes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
+require('mongoose').connect('mongodb://localhost:27017/kina', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
 const express = require('express');
+const userRoutes = require('./routes/users.route')
 const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all('/**', (req, res, next) => {
-  res.json(process.env);
-})
+app.use('/users', userRoutes);
 
 app.listen(4000, () => {
-  console.log(process.env.ENV);
+  console.log("App started");
 })
