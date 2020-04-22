23f520d4cf5318f93ced3a5ee6add9c9c8ebc197
DevGemmy <anthonia@nugitech.com>
2020-04-22 11:42:15 +0100

    created the schema with mongoose in the users.model fine.

---
+++ models/users.model.js
@@ -0,0 +1,9 @@
+const { Schema, ...mongoose } = require('mongooose');
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    age: { type: Number, required: true },
+    password: { type: String }
+});
+
+module.exports = mongoose.model('User', userSchema);
