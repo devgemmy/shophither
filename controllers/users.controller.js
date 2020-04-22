3912170e89503c5bf294bda8d8f907dc0139f8bf
DevGemmy <anthonia@nugitech.com>
2020-04-22 10:50:43 +0100

    added crud operations in the user.controller file

---
+++ controllers/users.controller.js
@@ -0,0 +1,43 @@
+const User = require('../models/users.model');
+
+module.exports = class UserController {
+    static async get(req, res, next) {
+        res.json(
+            await User.find()
+        )
+    }
+
+    static async getOne(req, res, next) {
+        res.json(
+            await User.findOne({
+                _id: req.params.user
+            })
+        )
+    }
+
+    static async create(req, res, next) {
+        res.json(
+            await User.create({
+                _id: req.params.user
+            })
+        )
+    }
+
+    static async update(req, res, next) {
+        res.json(
+            await User.updateOne({
+                _id: req.params.user
+            }, {
+                $set: req.body
+            })
+        )
+    }
+
+    static async delete(req, res, next) {
+        res.json(
+            await User.remove({
+                _id: req.params.user
+            })
+        )
+    }
+}
