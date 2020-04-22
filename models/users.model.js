6470588947079d54d9fdb1bfa1f59e3ced40f8bd
DevGemmy <anthonia@nugitech.com>
2020-04-22 13:32:36 +0100

    used the pre() method to save and check if a user exists and return a promise error if it does

--- models/users.model.js
+++ models/users.model.js
@@ -1,10 +1,29 @@
 const { Schema, ...mongoose } = require('mongooose');
+const User = require('../models/users.model');
 
 const userSchema = new Schema({
     username: { type: String, required: true },
-    age: { type: Number, required: true },
+    age: { type: Number, min: 15, required: true },
     password: { type: String },
     email: { type: String }
+}, {
+    timestamp: true
+});
+
+userSchema.pre('save', async function() {
+    //username validation
+    const userExists = await this.constructor.findOne({
+        username: this.username
+    });
+
+    if(userExists) {
+        return Promise((resolve, reject) => {
+            reject(new Error({username: "Username is already taken"}));
+        })
+    }
+    // age validation
+    // password validation
+    // email validation
 });
 
 module.exports = mongoose.model('User', userSchema);
